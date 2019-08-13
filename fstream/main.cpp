7502715abfaea8c4f4db84c42f2941b3374a46cb
zhoujf <zhoujf1636@163.com>
2019-08-13 15:15:59 +0800

    fstream

--- fstream/main.cpp
+++ fstream/main.cpp
@@ -7,9 +7,60 @@
 //
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void save_to_file()
+{
+    ofstream outfile("f2.dat");
+    if(! outfile)
+    {
+        cerr<<"open f2.dat error!"<<endl;
+        exit(1);
+    }
+    char c[80];
+    cin.getline(c,80);
+    for(int i = 0;c[i] != 0;i++)
+        if((c[i]>=65 && c[i]<=90) || (c[i]>=97 && c[i]<=122))
+        {
+            outfile.put(c[i]);
+            cout<<c[i];
+        }
+    cout<<endl;
+    outfile.close();
+}
+
+void get_from_file()
+{
+    char ch;
+    ifstream infile("f2.dat",ios::in);      // ios::nocreate不能用
+    if(! infile)
+    {
+        cerr <<"open f2.dat error!"<<endl;
+        exit(1);
+    }
+    ofstream outfile("f3.dat");
+    if(! outfile)
+    {
+        cerr <<"open f3.dat error!"<<endl;
+        exit(1);
+    }
+    while(infile.get(ch))
+    {
+        if(ch>=97 && ch<=122)
+            ch = ch - 32;
+        outfile.put(ch);
+        cout<<ch;
+    }
+    cout<<endl;
+    infile.close();
+    outfile.close();
+}
+
+
+int main(int argc, const char * argv[])
+{
+    save_to_file();
+    get_from_file();
     return 0;
 }
